d43a99d55352f2c54bad8512e80c3fe542ad23ba
Robert DJ <git@dahl-jacobsen.dk>
2018-11-18 21:10:22 +0100

    Update Project.toml

--- src/AzureBlob.jl
+++ src/AzureBlob.jl
@@ -4,11 +4,14 @@ using Nettle
 using Codecs
 using Dates
 using Pipe
+using HTTP
 
 export 
     azure_signature,
+    get_blob,
     http_date
 
+include("rest.jl")
 include("signature.jl")
 
 end # module

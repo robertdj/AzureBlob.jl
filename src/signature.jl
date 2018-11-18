642065326ce7c55f64c6f97cd12660e899fd76b0
Robert DJ <git@dahl-jacobsen.dk>
2018-11-18 12:45:22 +0100

    Azure storage signature
    that appears to be correct.

--- src/signature.jl
+++ src/signature.jl
@@ -1,5 +1,5 @@
 """
-Return the content size of an object as needed in `azure_signature`.
+Return the content size of an object as needed in [`azure_signature`](@ref).
 
 # Examples
 ```jldoctest
@@ -12,11 +12,38 @@ contentsize(obj::String) = length(obj)
 """
 Print `DateTime` in RFC 1123 format as requireddby the REST interface.
 
-Check
-<https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings>
-for info.
+Check <https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings> for info.
 """
-function http_date(dt::DateTime)
-    Dates.format(dt, Dates.RFC1123Format)
+http_date(dt::DateTime) = Dates.format(dt, Dates.RFC1123Format)
+
+
+"""
+Generate signature for Azure storage.
+"""
+function azure_signature(url, verb, storageaccount, storagekey,
+                         container, datestamp, headers = "", CMD = "",
+                         contentsize = "", 
+                         contenttype = "application/json")
+    time_arg = string("x-ms-date:", datestamp, "\nx-ms-version:2017-04-17")
+    if length(headers) > 0
+        time_arg = string(headers, "\n", time_arg)
+    end
+
+    location_arg = string("/", storageaccount, "/", container, CMD)
+
+    signature = string(verb, "\n\n\n", contentsize, "\n\n", contenttype, 
+                       "\n\n\n\n\n\n\n", time_arg, "\n", location_arg)
+
+    #= Base64.base64decode(storagekey) =#
+    a = Nettle.digest(
+        "sha256", 
+        #= Base64.base64decode(storagekey), =#
+        Codecs.decode(Codecs.Base64, storagekey),
+        signature
+    )
+    b = Codecs.encode(Codecs.Base64, a)
+    String(b)
+    #= Base64.base64encode() =#
+    #= x -> StringEncoding(x, "UTF-8") #|> =#
 end
 

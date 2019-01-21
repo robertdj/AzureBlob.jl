3f5d65aaa9559bfb89be3eb3f358fffd6b4babce
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2019-01-21 13:40:51 +0100

    Remove warning in test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,19 +2,19 @@ using AzureBlob
 using Dates
 using Test
 
-@testset "Black-box signature" begin
+@testset "Black box signature" begin
 	timestamp = DateTime(2019, 1, 1, 12, 34, 56) |> http_date
 
-	# TODO: Base64 encoding gives warning
+	# Length of storagekey must be a multiple of 4
 	sig = azure_signature(
 		url = "url",
 		verb = "GET",
 		storageaccount = "storageaccount",
-		storagekey = "storagekey",
+		storagekey = "storagekeyxx",
 		container = "container",
 		timestamp = timestamp
 	)
 
-	@test sig == "VQvVWSwxbKV/1SJEfmYVIreNyQjc6SjAJtKABLymPcQ="
+	@test sig == "3uLSOeEgZxolreoOtEgCe8kUUyfK9lvaKPeYoAdjQ7Q="
 end
 

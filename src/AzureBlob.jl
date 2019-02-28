85a5a4d71097b82c3e03747989107f04fca62459
Robert DJ <git@dahl-jacobsen.dk>
2019-02-28 21:25:40 +0100

    Add local time zone and UTC as constants

--- src/AzureBlob.jl
+++ src/AzureBlob.jl
@@ -4,6 +4,7 @@ import Base64
 import Dates
 import HTTP
 import Nettle
+import TimeZones
 
 using Pipe
 
@@ -16,6 +17,13 @@ const X_MS_VERSION = "2017-04-17"
 
 # Dates.RFC1123Format with " GMT" in the end
 const RFC1123_GMT = Dates.DateFormat("e, dd u yyyy HH:MM:SS G\\MT")
+
+const UTC = TimeZones.tz"UTC"
+const LOCALZONE = try
+		TimeZones.localzone()
+	catch
+		UTC
+	end
 
 include("rest.jl")
 include("signature.jl")

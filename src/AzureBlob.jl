584a864898030a4f746f7b1bee3528a8ef8ffc2f
Robert DJ <git@dahl-jacobsen.dk>
2019-03-28 21:25:16 +0100

    Move constants into separate file

--- src/AzureBlob.jl
+++ src/AzureBlob.jl
@@ -16,17 +16,7 @@ export
 	storage_signature
 
 
-const X_MS_VERSION = "2017-04-17"
-
-# Dates.RFC1123Format with " GMT" in the end
-const RFC1123_GMT = Dates.DateFormat("e, dd u yyyy HH:MM:SS G\\MT")
-
-const UTC = TimeZones.tz"UTC"
-#
-# In some virtualized environments it is not possible to get the local timezone
-const LOCALZONE = try TimeZones.localzone() catch; UTC end
-
-
+include("constants.jl")
 include("rest.jl")
 include("signature.jl")
 
